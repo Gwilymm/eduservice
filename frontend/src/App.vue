c27c7df20e6baadc91e8b5cb15ac9cf675b805db
G8ite <amandine.pda@gmail.com>
2025-03-15 12:46:16 +0100

    fix(frontend): Corrige la largeur des pages sur App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <v-app>
-    <v-navigation-drawer app color="primary" class="w-25">
+    <v-navigation-drawer app color="primary" width="300" permanent>
       <div class="d-flex flex-column justify-between h-100">
         <div class="d-flex justify-center mb-8 mt-10">
-          <v-img :src="logo" max-width="250"  contain></v-img>
+          <v-img :src="logo" max-width="250" contain></v-img>
         </div>
         <v-container v-if="showYearSelector">
-          <v-select v-model="selectedYear" :item="schoolYears" label="Année scolaire" outlined dense>
+          <v-select v-model="selectedYear" :items="schoolYears" label="Année scolaire" outlined dense>
           </v-select>
         </v-container>
         <v-spacer></v-spacer>
@@ -21,8 +21,11 @@
         </v-container>
       </div>
     </v-navigation-drawer>
-    <v-main>
-      <RouterView />
+
+    <v-main class="main-content">
+      <v-container fill-height fluid>
+        <RouterView />
+      </v-container>
     </v-main>
   </v-app>
 </template>
@@ -37,12 +40,11 @@ import mds from '@/assets/images/logo_mds_blanc.png';
 import ihecf from '@/assets/images/logo_ihecf_blanc.png';
 import tunon from '@/assets/images/logo_tunon_blanc.png';
 
-
 import { ref, computed } from 'vue';
 import { useRoute } from 'vue-router';
 import { RouterView } from 'vue-router';
 
-const route = useRoute()
+const route = useRoute();
 
 const currentYear = new Date().getFullYear();
 
@@ -50,37 +52,21 @@ const schoolYears = [
   `${currentYear - 2} / ${currentYear - 1}`,
   `${currentYear - 1} / ${currentYear}`,
   `${currentYear} / ${currentYear + 1}`,
-]
+];
 
-const selectedYear = ref(schoolYears[-1])
+const selectedYear = ref(schoolYears[schoolYears.length - 1]);
 
 const showYearSelector = computed(() => {
   const pagesWithSelector = ['/home', '/missions', '/administration', '/'];
   return pagesWithSelector.includes(route.path);
 });
-
 </script>
 
 <style>
-@font-face {
-  font-family: 'HomemadeApple';
-  src: url('@/assets/fonts/HomemadeApple-Regular.woff') format("woff");
+.theme--light.v-application {
+  background: #FAFAFA;
 }
-
-@font-face {
-  font-family: 'NeoSansStd';
-  src: url('@/assets/fonts/Neo-Sans-Std-Medium.woff') format("woff");
-}
-
-body {
-  font-family: NeoSansStd;
-}
-
-h1 {
-  font-family: HomemadeApple;
-}
-
-.navigation-drawer-background {
-  background-color: var(--dark-blue);
+.main-content {
+  overflow-y: hidden;
 }
 </style>
