26c278e1f7b08d533f69afc42ab78f5228d921b4
Amandine Pimentel de Andrade <amandine@spirkop.com>
2025-05-22 21:23:26 +0200

    fix(frontend): Resout le conflit de homeview

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -147,34 +147,20 @@ const snackbar = ref({
 async function fetchUser() {
   loading.value = true
   try {
-<<<<<<< HEAD
-    const userData = await getCurrentUser()
-    console.log('API user data:', userData)
-    user.value = userData
-    snackbar.value = { show: true, text: 'Données chargées', color: 'success' }
-  } catch (error) {
-    console.error('Error loading user data:', error)
-    snackbar.value = { show: true, text: 'Erreur chargement user', color: 'error' }
-=======
     user.value = await getCurrentUser()
     // snackbar.value = { show: true, text: 'Données chargées', color: 'success' }
   } catch {
     snackbar.value = { show: true, text: 'Erreur de chargement des données de l\'utiliateur', color: 'error' }
->>>>>>> origin/frontend-fixing-amandine
   } finally {
     loading.value = false
   }
 }
 
-<<<<<<< HEAD
-const fileUrl = ref('https://example.com/autorisation_diffusion.pdf')
-=======
 const displayDate = computed(() => {
   return user.value?.updatedPointsDate || new Date()
 })
 
 const fileUrl = ref(autorisationPdf)
->>>>>>> origin/frontend-fixing-amandine
 
 const openPdfInNewTab = () => {
   window.open(fileUrl.value, '_blank', 'noopener,noreferrer')
